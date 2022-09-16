e6f50e825dd05502506ef69d99e4cf180f5aa484
peytonrb <112011839+peytonrb@users.noreply.github.com>
2022-09-16 00:40:51 +0000

    Sep 15, 2022, 8:40 PM

--- Assets/Scripts/enemyRotator.cs
+++ Assets/Scripts/enemyRotator.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class enemyRotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
