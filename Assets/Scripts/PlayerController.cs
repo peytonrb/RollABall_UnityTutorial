601dc3a13d5c52a5d51b5de3b95226c4c2d790eb
peytonrb <112011839+peytonrb@users.noreply.github.com>
2022-09-16 02:37:38 +0000

    Sep 15, 2022, 10:37 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        lives = 3;
         SetCountText();
         winTextObject.SetActive(false);
     }
